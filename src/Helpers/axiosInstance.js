d03c18ccfbf36179f83280b967480c56da19399c
AbhayBamboriya <bamboriya09@gmail.com>
2025-01-07 09:07:05 +0530

    .env addedd

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // const   BASE_URL='http://localhost:4052/api/v1'
-const BASE_URL=process.env.URL
+const BASE_URL=process.env.URL1
 // const BASE_URL='https://lms-server-10l8.onrender.com/api/v1'
 // const changePasswordUrl='localhost:4051/api/v1/user/password'
 const axiosInstance=axios.create(); 
