23e746eca3e1bd8af541be28fd55121f487e109b
AbhayBamboriya <bamboriya09@gmail.com>
2025-02-11 02:20:54 +0530

    url changed

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL="https://lms-server-10l8.onrender.com"
+const BASE_URL="https://lms-server-10l8.onrender.com/api/v1"
 
 const  axiosInstance=axios.create(); 
 
