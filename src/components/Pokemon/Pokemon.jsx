43d695435ce896a4d09872276914a3e5defb6c5b
AbhayBamboriya <bamboriya09@gmail.com>
2025-02-11 00:48:39 +0530

    revert small changes

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -6,9 +6,9 @@ function Pokemon({name,image,id}){
     
     return (
         <div className='pokemon'>
-            <Link to={`/pokemonName/${name}`} onClick={<Loader/>}>
-                <div className='pokemon-name'> {name}</div>
-                <div><img className="pokemon-image" src={image}/></div>
+            <Link to={`/pokemon/id`} onClick={<Loader/>}>
+            <div className='pokemon-name'> {name}</div>
+            <div><img className="pokemon-image" src={image}/></div>
             </Link>
         </div>
     )
