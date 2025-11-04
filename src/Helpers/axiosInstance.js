290ba389ed242c79bbf5e54512d16a9c47ced7ec
AbhayBamboriya <bamboriya09@gmail.com>
2025-11-05 00:47:22 +0530

    URL changed

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -4,6 +4,6 @@ const BASE_URL="https://lms-server-10l8.onrender.com/api/v1"
 
 const  axiosInstance=axios.create(); 
 
-axiosInstance.defaults.baseURL=BASE_URL
+axiosInstance.defaults.baseURL=process.env.BASE_URL
 axiosInstance.defaults.withCredentials=true
 export default axiosInstance
